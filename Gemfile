fcb211e5c51f7b2a823789debdaa217df714b49f
Michiel Koning <michielkoning@gmail.com>
2016-11-20 16:41:49 +0100

    clean up

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-gem 'jekyll', '~> 2.5.2'
-gem 'sass', '>= 3.3'
-gem 'scss-lint', '~> 0.31.0'
--- gulp/tasks/development/files.js
+++
@@ -1,7 +0,0 @@
-var gulp   = require('gulp');
-var config = require('../../config').files;
-
-gulp.task('files', function() {
-  return gulp.src(config.files)
-    .pipe(gulp.dest(config.dest));
-});
